b492538e98c29062e58f8575ef49d0d665a6bf9d
Paula <p.villagraga@gmail.com>
2022-09-26 02:31:16 -0300

    Ramas modificadas

--- prueba2/settings.py
+++ prueba2/settings.py
@@ -37,11 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
     'firstApp',
-=======
     'secondApp',
->>>>>>> app2
 ]
 
 MIDDLEWARE = [
